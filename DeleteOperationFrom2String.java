3d250818856bfee2e5936432ace1bb6c769b4019
Artur Ardai <krkerdo@gmail.com>
2022-06-14 10:13:34 -1200

    ye

--- DeleteOperationFrom2String.java
+++ DeleteOperationFrom2String.java
@@ -1,8 +1,28 @@
+import javax.lang.model.util.ElementScanner14;
+
 public class DeleteOperationFrom2String {
     public static void main(String[] args) {
         
     }
+    public int minDistance2(String word1, String word2) {
+        int [][]dp=new int [word1.length()+1][word2.length()+1];
+        for (int i = 0; i <=word1.length(); i++) {
+            for (int j = 0; j <=word2.length(); j++) {
+                if(i==0||j==0)
+                continue;
+                
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                dp[i][j]=dp[i-1][j-1]+1;
+                else 
+                dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return word1.length()+word2.length()-2*dp[word1.length()][word2.length()];
+
+    }
+
     public int minDistance(String word1, String word2) {
+      //wrong :(
         int db=0;
         int []v1=new int[26];
         int []v2=new int[26];
