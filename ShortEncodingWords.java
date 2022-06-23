c2d06dd41e0ae228da079ae0a318cf4570040346
Artur Ardai <krkerdo@gmail.com>
2022-06-22 18:44:26 -1200

    ye

---
+++ ShortEncodingWords.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ShortEncodingWords {
+    public static void main(String[] args) {
+        
+    }
+    public  static int minimumLengthEncoding(String[] words) {
+        Set<String> good = new HashSet(Arrays.asList(words));
+        for (String word: words) {
+            System.out.println(word);
+            for (int k = 1; k < word.length(); ++k)
+                {
+                 good.remove(word.substring(k));
+                 System.out.println(word.substring(k));   
+                } 
+            }
+
+        int ans = 0;
+        for (String word: good)
+            ans += word.length() + 1;
+        return ans;
+    }
+}
