9b3977c5b67ab9b5c37bca57cfa0a1b48604fa2e
Artur Ardai <krkerdo@gmail.com>
2022-06-22 18:57:41 -1200

    new

--- ShortEncodingWords.java
+++ ShortEncodingWords.java
@@ -4,16 +4,17 @@ import java.util.Set;
 
 public class ShortEncodingWords {
     public static void main(String[] args) {
-        
+        String []s={"time", "me", "bell"};
+        minimumLengthEncoding(s);
     }
     public  static int minimumLengthEncoding(String[] words) {
         Set<String> good = new HashSet(Arrays.asList(words));
         for (String word: words) {
-            System.out.println(word);
+            System.out.println(word+" word");
             for (int k = 1; k < word.length(); ++k)
                 {
                  good.remove(word.substring(k));
-                 System.out.println(word.substring(k));   
+                 System.out.println(word.substring(k)+" substring");   
                 } 
             }
 
