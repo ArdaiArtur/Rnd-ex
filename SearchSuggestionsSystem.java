6ebc42c6618ab444045c2d7df694000f5e7e0ab8
Artur Ardai <krkerdo@gmail.com>
2022-06-23 16:50:17 -1200

    yeah baby

--- SearchSuggestionsSystem.java
+++ SearchSuggestionsSystem.java
@@ -13,15 +13,15 @@ public class SearchSuggestionsSystem {
         List<List<String>> fin=new ArrayList<>();
         List<String> sub=new ArrayList<String>();
         Arrays.sort(products);
-        for (int i = 0; i <=searchWord.length(); i++) {
+        for (int i = 1; i <=searchWord.length(); i++) {
             String s= searchWord.substring(0, i);
             int count=0;
             int numb=0;
             while(count<3&&numb<products.length)
             {
                 
-                if(products[numb].length()>i&&products[numb].substring(0, i).equals(s))
-                {
+                if(products[numb].length()>=i&&products[numb].substring(0, i).equals(s))
+                {System.out.println(products[numb]+" and "+products[numb].substring(0, i)+" and s="+s );
                     sub.add(products[numb]);
                 count++;
                 }
