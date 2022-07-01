36a280cd93afdc8fa70f1b362c4ec8de85f5ee10
Artur Ardai <krkerdo@gmail.com>
2022-07-01 11:57:16 -1200

    yes

--- MinDeletionsToMakeCharacterFrequenciesUnique.java
+++ MinDeletionsToMakeCharacterFrequenciesUnique.java
@@ -1,11 +1,11 @@
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class MinDeletionsToMakeCharacterFrequenciesUnique {
   public static void main(String[] args) {
-    
+    String s="aaabbbcc";
+    minDeletions(s);
   } 
-  public int minDeletions(String s) {
+  public static int minDeletions(String s) {
         int[] ar=new int[24];
         for (char c : s.toCharArray()) {
             ar[c-'a']++;
@@ -22,12 +22,13 @@ public class MinDeletionsToMakeCharacterFrequenciesUnique {
             {
                 while(hs.contains(i) || i>0)
                 {
+                    System.out.println(i);
                     i--;
                     db++;
                 }
                 hs.add(i);
             }
         }
-        return db;
+        return db-1;
 }
 }
