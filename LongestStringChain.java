cc8b4baf2f3eca835af5a24d7d9945b6d06a2ad9
Artur Ardai <krkerdo@gmail.com>
2022-07-03 13:45:35 -1200

    yes

---
+++ LongestStringChain.java
@@ -0,0 +1,12 @@
+/**
+ * LongestStringChain
+ */
+public class LongestStringChain {
+
+    public static void main(String[] args) {
+        
+    }
+    public int longestStrChain(String[] words) {
+        
+    }
+}
